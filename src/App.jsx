285987fc87110cd0a53261f8bfb6b9f4860b8bc8
pelinergul <pelinxer@gmail.com>
2023-10-04 17:51:15 +0300

    calisma bitti

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,56 @@
+import '../node_modules/bulma/css/bulma.css'
 import './App.css'
-import { Course } from './components/Course'
+import Course from './components/Course'
+import Angular from './assets/angular.jpg'
+import Bootstrap from './assets/bootstrap5.png'
+import Csharp from './assets/ccsharp.png'
+import KompleWeb from './assets/kompleweb.jpg'
 
 function App() {
 
   return (
     <div className='App'>
-      <Course />
-      <Course />
-      <Course />
-      <Course />
+      <section className="hero is-link">
+        <div className="hero-body">
+          <p className="title">
+            Kurslarım
+          </p>
+        </div>
+      </section>
+      <div className='container'>
+        <section className='section'>
+        <div className='columns'>
+          <div className='column'>
+          <Course 
+          image={Angular}
+          title="Angular" 
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, maxime nulla vero accusamus eaque praesentium veritatis commodi animi neque corporis."
+          />
+          </div>
+          <div className='column'>
+          <Course 
+          image={Bootstrap}
+          title="Bootstrap" 
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, maxime nulla vero accusamus eaque praesentium veritatis commodi animi neque corporis."
+          />
+          </div>
+          <div className='column'>
+          <Course 
+          image={Csharp}
+          title="Csharp" 
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, maxime nulla vero accusamus eaque praesentium veritatis commodi animi neque corporis."
+          />
+          </div>
+          <div className='column'>
+          <Course 
+          image={KompleWeb}
+          title="Komple Web" 
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, maxime nulla vero accusamus eaque praesentium veritatis commodi animi neque corporis."
+          />
+          </div>
+        </div>
+        </section>
+      </div>
     </div>
   )
 } 
--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
-export const Course = () => {
-  return (
-    <div>Course</div>
-  )
+export default function Course({image,title,description}) {
+    // console.log(props);
+    // const title = props.title;
+    // const description=props.description;
+    // const {title, description} = props
+    return (
+        <>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-4by3">
+                    <img src={image} alt="Placeholder image" />
+                </figure>
+            </div>
+            <div class="media-content">
+                <p class="title is-4">{title}</p>
+            </div>
+            <div className="card-content">
+                {description}
+            </div>
+        </div>
+        </>
+    )
 }
